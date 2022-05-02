8f4a902e48fc2d0bcd0275eb0f5c46f6b7c58442
liumingmin <liumingmin@kingsoft.com>
2022-05-02 20:25:58 +0800

    feat: cdService more flexible

--- cd_service.go
+++ cd_service.go
@@ -5,52 +5,52 @@ import (
 	"strings"
 )
 
-//todo 参数统一为map[string]string, 详细参数Default里面传
 type CdService struct {
-	name       string
-	pkgUrl     string            // 程序包名，defaultScript仅支持tgz格式程序包
-	targetPath string            // 服务部署目标目录
-	runCmd     string            // 启动文件或命令
-	envVar     map[string]string // 动态参数-通过环境变量传递
-	cdScript   *CdScript
+	name          string            // service name 服务名
+	params        map[string]string // invoke script dynamic params,see cdScript 传入脚本的动态参数与cdScript定义必须一致
+	cdScript      *CdScript         // deploy script  部署脚本
+	deployCounter uint32            // deploy counter 部署计数器，用于并发部署和计数
+}
 
-	deployCounter uint32
+func NewCdService(name string, params map[string]string, cdScript *CdScript) *CdService {
+	return &CdService{
+		name:     name,
+		params:   params,
+		cdScript: cdScript,
+	}
 }
 
 //程序运行配置中，抽提db信息放到环境变量中运行时传递
 //不同环境的配置文件直接写入程序包,动态内容使用环境变量设置
+//pkgUrl     string            // 程序包名，defaultScript仅支持tgz格式程序包
+//targetPath string            // 服务部署目标目录
+//runCmd     string            // 启动文件或命令
+//envVar     map[string]string // 动态参数-通过环境变量传递
+
 func NewDefaultCdService(name, pkgUrl, targetPath, runCmd string, envVar map[string]string) *CdService {
-	if envVar == nil {
-		envVar = make(map[string]string)
+	var envsStr strings.Builder
+	if envVar != nil {
+		for key, value := range envVar {
+			envsStr.WriteString(fmt.Sprintf(" %v=%v", key, value))
+		}
 	}
-	return &CdService{
-		name:       name,
-		pkgUrl:     pkgUrl,
-		targetPath: targetPath,
-		runCmd:     runCmd,
-		envVar:     envVar,
-		cdScript:   NewDefaultCdScript(),
+
+	cdService := &CdService{
+		name: name,
+		params: map[string]string{
+			"PKG_URL":     pkgUrl, //s3get download package
+			"TARGET_PATH": targetPath,
+			"RUN_CMD":     runCmd,
+			"ENV_VAR":     envsStr.String(),
+		},
+		cdScript: NewDefaultCdScript(),
 	}
+
+	return cdService
 }
 
 func (t *CdService) Name() string {
 	return t.name
-}
-
-func (t *CdService) PkgUrl() string {
-	return t.pkgUrl
-}
-
-func (t *CdService) TargetPath() string {
-	return t.targetPath
-}
-
-func (t *CdService) RunCmd() string {
-	return t.runCmd
-}
-
-func (t *CdService) EnvVar() map[string]string {
-	return t.envVar
 }
 
 func (t *CdService) BindScript(cdScript *CdScript) {
@@ -62,17 +62,10 @@ func (t *CdService) GetCdTaskScriptConfig(hostIp string) (string, error) {
 }
 
 func (t *CdService) GetParams() map[string]string {
-	//动态参数
-	envVar := t.EnvVar()
-	var envsStr strings.Builder
-	for key, value := range envVar {
-		envsStr.WriteString(fmt.Sprintf(" %v=%v", key, value))
-	}
+	return t.params
+}
 
-	return map[string]string{
-		"PKG_URL":     t.PkgUrl(), //s3get download package
-		"TARGET_PATH": t.TargetPath(),
-		"RUN_CMD":     t.RunCmd(),
-		"ENV_VAR":     envsStr.String(),
-	}
+//update like PKG_URL
+func (t *CdService) UpdateParam(paramKey, paramValue string) {
+	t.params[paramKey] = paramValue
 }
